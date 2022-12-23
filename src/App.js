a2402c07d3d99dad6085e9e356cdef419c76304b
Erick Milan <erickmilan1002@gmail.com>
2022-12-23 00:30:33 -0500

    add top panel on the main screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
-import 
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./scenes/global/TopBar";
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-     
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
